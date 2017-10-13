cb2a763a8ae5e4f20b5d29cc85aa6223e503c618
Luigi <luigim92@gmail.com>
2017-10-13 18:16:26 -0400

    Added moles and adjusted answer size Can't repeatedly click on same answer Added score and timer
    TODO Think about supported android version and change code accordingly
    TODO Measure time better

--- app/src/main/java/com/android/tesis/luigi/whackac/gameActivity.java
+++ app/src/main/java/com/android/tesis/luigi/whackac/gameActivity.java
@@ -17,6 +17,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.Random;
 import java.util.Timer;
@@ -37,12 +38,23 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton button;
     ImageButton button2;
     ImageButton button3;
+    ImageView syntaxImg1;
+    ImageView syntaxImg2;
+    ImageView syntaxImg3;
+    ImageView moleImg1;
+    ImageView moleImg2;
+    ImageView moleImg3;
+    TextView scoreText;
+    TextView timeText;
     Timer timer= new Timer();
     int flag;
     int imgused1;
     int imgused2;
     int imgused3;
     int currvalue;
+    boolean touched1 = false;
+    boolean touched2 = false;
+    boolean touched3 = false;
     Handler handler = new Handler();
 
 
@@ -58,11 +70,19 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_game);
 
         button=(ImageButton) findViewById(R.id.imageButton);
-
         button2=(ImageButton) findViewById(R.id.imageButton2);
-
         button3=(ImageButton) findViewById(R.id.imageButton3);
 
+        syntaxImg1 = (ImageView) findViewById(R.id.syntax1);
+        syntaxImg2 = (ImageView) findViewById(R.id.syntax2);
+        syntaxImg3 = (ImageView) findViewById(R.id.syntax3);
+
+        moleImg1 = (ImageView) findViewById(R.id.mole1);
+        moleImg2 = (ImageView) findViewById(R.id.mole2);
+        moleImg3 = (ImageView) findViewById(R.id.mole3);
+
+        scoreText = (TextView) findViewById(R.id.scoretext);
+        timeText = (TextView) findViewById(R.id.timetext);
 
         button.setOnClickListener(this);
         button2.setOnClickListener(this);
@@ -90,9 +110,14 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
                     /*if (currvalue != imgused1) {
                         Log.d("1)currvalue != imgused1","["+currvalue+","+"("+imgused1+","+counter1+")"+"]");
                     }*/
-                    button.setBackgroundResource(img[currvalue]);
-                    imgused1 = currvalue;
+                    /*syntaxImg1.setBackgroundResource(img[currvalue]);*/
 
+                    touched1 = false;
+                    moleImg1.setImageDrawable(getResources().getDrawable(R.drawable.moleneutral));
+
+                    /*syntaxImg1.setImageDrawable(getResources().getDrawable(img[currvalue],null));*/
+                    syntaxImg1.setImageDrawable(getResources().getDrawable(img[currvalue]));
+                    imgused1 = currvalue;
                     counter1 = ThreadLocalRandom.current().nextInt(3,6);
 
                 } else {
@@ -112,7 +137,12 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
                         Log.d("2)currvalue != imgused1","["+currvalue+","+"("+imgused1+","+counter1+")"+"]");
                     }*/
 
-                    button2.setBackgroundResource(img[currvalue]);
+                    touched2 = false;
+                    moleImg2.setImageDrawable(getResources().getDrawable(R.drawable.moleneutral));
+
+                    /*syntaxImg2.setBackgroundResource(img[currvalue]);*/
+                    syntaxImg2.setImageDrawable(getResources().getDrawable(img[currvalue]));
+
                     imgused2 = currvalue;
 
                     counter2 = ThreadLocalRandom.current().nextInt(3,6);
@@ -138,7 +168,11 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
                         Log.d("3)currvalue != imgused2","["+currvalue+","+"("+imgused2+","+counter2+")"+"]");
                     }*/
 
-                    button3.setBackgroundResource(img[currvalue]);
+                    touched3 = false;
+                    moleImg3.setImageDrawable(getResources().getDrawable(R.drawable.moleneutral));
+
+                    /*syntaxImg3.setBackgroundResource(img[currvalue]);*/
+                    syntaxImg3.setImageDrawable(getResources().getDrawable(img[currvalue]));
                     imgused3 = currvalue;
 
                     counter3 = ThreadLocalRandom.current().nextInt(3,6);
@@ -151,6 +185,8 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
 
                 if (countdown>0) {
                     countdown -= 1;
+                    scoreText.setText("Puntuacion: "+score);
+                    timeText.setText("Tiempo: "+countdown/2);
                     handler.postDelayed(changeImage, 500);
                 } else {
                     Toast.makeText( getApplicationContext(),"Timeout! Puntuacion:"+score ,Toast.LENGTH_SHORT).show();
@@ -175,42 +211,58 @@ public class gameActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()) {
             case R.id.imageButton:
-
-              int a;
-                for(a=0;a<imgcorrecta.length;a++){
-                if(img[imgused1] == imgcorrecta[a]){
-                    Toast.makeText( getApplicationContext(),"Correcto!",Toast.LENGTH_SHORT).show();
-                    score += 5;
-                    }else{
-                    Toast.makeText( getApplicationContext(),"NOPE!",Toast.LENGTH_SHORT).show();
-                    score -= 2;
+                int a;
+                if (!touched1) {
+                    touched1 = true;
+                    counter1 = 2;
+                    for (a = 0; a < imgcorrecta.length; a++) {
+                        if (img[imgused1] == imgcorrecta[a]) {
+                            //Toast.makeText(getApplicationContext(), "Correcto!", Toast.LENGTH_SHORT).show();
+                            moleImg1.setImageDrawable(getResources().getDrawable(R.drawable.moleright));
+                            score += 5;
+                        } else {
+                            //Toast.makeText(getApplicationContext(), "NOPE!", Toast.LENGTH_SHORT).show();
+                            moleImg1.setImageDrawable(getResources().getDrawable(R.drawable.molewrong));
+                            score -= 2;
+                        }
                     }
                 }
                 break;
 
-
-           case R.id.imageButton2:
-               int b;
-               for(b=0;b<imgcorrecta.length;b++){
-                   if(img[imgused2] == imgcorrecta[b]){
-                       Toast.makeText( getApplicationContext(),"Correcto!",Toast.LENGTH_SHORT).show();
-                       score += 5;
-                   }else{
-                       Toast.makeText( getApplicationContext(),"NOPE!",Toast.LENGTH_SHORT).show();
-                       score -= 2;
-                   }
-               }
-
+            case R.id.imageButton2:
+                int b;
+                if (!touched2) {
+                    touched2 = true;
+                    counter2 = 2;
+                    for (b = 0; b < imgcorrecta.length; b++) {
+                        if (img[imgused2] == imgcorrecta[b]) {
+                            //Toast.makeText(getApplicationContext(), "Correcto!", Toast.LENGTH_SHORT).show();
+                            moleImg2.setImageDrawable(getResources().getDrawable(R.drawable.moleright));
+                            score += 5;
+                        } else {
+                            //Toast.makeText(getApplicationContext(), "NOPE!", Toast.LENGTH_SHORT).show();
+                            moleImg2.setImageDrawable(getResources().getDrawable(R.drawable.molewrong));
+                            score -= 2;
+                        }
+                    }
+                }
                 break;
+
             case R.id.imageButton3:
                 int c;
-                for(c=0;c<imgcorrecta.length;c++){
-                    if(img[imgused3] == imgcorrecta[c]){
-                        Toast.makeText( getApplicationContext(),"Correcto!",Toast.LENGTH_SHORT).show();
-                        score += 5;
-                    }else{
-                        Toast.makeText( getApplicationContext(),"NOPE!",Toast.LENGTH_SHORT).show();
-                        score -= 2;
+                if (!touched3) {
+                    touched3 = true;
+                    counter3 = 2;
+                    for (c = 0; c < imgcorrecta.length; c++) {
+                        if (img[imgused3] == imgcorrecta[c]) {
+                            //Toast.makeText(getApplicationContext(), "Correcto!", Toast.LENGTH_SHORT).show();
+                            moleImg3.setImageDrawable(getResources().getDrawable(R.drawable.moleright));
+                            score += 5;
+                        } else {
+                            //Toast.makeText(getApplicationContext(), "NOPE!", Toast.LENGTH_SHORT).show();
+                            moleImg3.setImageDrawable(getResources().getDrawable(R.drawable.molewrong));
+                            score -= 2;
+                        }
                     }
                 }
                 break;
